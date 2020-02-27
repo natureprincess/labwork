b1bae139b086f955dad9e14b20b8cc1461964ab0
soumya <soumyamadhurikrishna@gmail.com>
2020-02-27 15:26:34 +0530

    greater

---
+++ labwork/greater.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "find the greatest number"
+echo "enter first number"
+read num1
+echo "enter second number"
+read num2
+if [ $num1 -gt $num2 ]
+then
+echo "$num1 is greater"
+else
+echo "$num2 is greater"
+fi
+
